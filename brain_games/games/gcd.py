f7313d10a5710af3db6b888ea3e7f07b3c969192
Konstantin Faraktinov <frktnv@protonmail.com>
2023-02-02 22:01:52 +0300

    Make refactoring for gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,14 +6,9 @@ task = 'Find the greatest common divisor of given numbers.'
 def get_question():
     num_1 = random.randint(1, 99)
     num_2 = random.randint(1, 99)
-    min_number = min(num_1, num_2)
-    max_number = max(num_1, num_2)
 
-    if max_number % min_number == 0:
-        answer = min_number
-    else:
-        for x in range(1, min_number // 2 + 1):
-            if (min_number % x == 0) and (max_number % x == 0):
-                answer = x
+    for x in range(1, min(num_1, num_2) + 1):
+        if (num_1 % x == 0) and (num_2 % x == 0):
+            answer = x
 
     return (f'{num_1} {num_2}', str(answer))
